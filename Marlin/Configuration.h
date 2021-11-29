31b89b9cb5dd6191b59ffe618046eff6dc3ab548
Andrey <a.v,grigorenko.mail@gmail.com>
2021-11-29 16:23:18 +0300

    Калибровка ПИД

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -601,9 +601,9 @@
     #define DEFAULT_Ki_LIST {   1.54,   1.54 }
     #define DEFAULT_Kd_LIST {  76.55,  76.55 }
   #else
-    #define DEFAULT_Kp  21.73
-    #define DEFAULT_Ki   1.54
-    #define DEFAULT_Kd  76.55
+    #define DEFAULT_Kp  24.03
+    #define DEFAULT_Ki   2.04
+    #define DEFAULT_Kd  70.08
   #endif
 #endif // PIDTEMP
 
